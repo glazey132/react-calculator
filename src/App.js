af0e4944d3a958dca479f1498aa2b0ccba2b0000
Alex <alglaze132@gmail.com>
2018-08-20 10:54:13 -0700

    test heroku

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      socket: io('ws://localhost:8080', { transports: ['websocket'] }),
+      socket: io('ws://react-calculator-69586.herokuapp.com/', {
+        transports: ['websocket']
+      }),
       calculations: []
     };
 

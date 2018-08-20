50545f8883a4c39be3177be693f828ac8b7ee238
Alex <alglaze132@gmail.com>
2018-08-19 21:51:15 -0700

    npm install, calculator component and html section

---
+++ src/Calculator.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  render() {
+    return <div className="">{/* <Display /> */}</div>;
+  }
+}
+
+export default Calculator;

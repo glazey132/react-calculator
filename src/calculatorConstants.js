4b21bbfb5a47ce495c6b3c3a2db663fdb00e45ce
glazey132 <alglaze132@gmail.com>
2019-11-23 23:43:13 -0800

    bare bone server

--- src/calculatorConstants.js
+++
@@ -1,9 +0,0 @@
-
-const calculatorConstants = {
-    nums: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'],
-    operators: ['+', '-', '*', '/'],
-    fns: ['C', '='],
-    calculatorOrder: ['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '0', '.', 'C', '/', '='],
-};
-
-export default calculatorConstants;
